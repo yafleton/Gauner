af770ab9785203616948446479e77cd9d113a28e
yafleton <yafleton@example.com>
2025-10-03 19:59:17 +0200

    Fix voice selection - Voice and language dropdowns now work again - getAvailableVoices method properly implemented with full voice list

--- src/services/azureTTSWorker.ts
+++ src/services/azureTTSWorker.ts
@@ -107,7 +107,60 @@ export class AzureTTSWorkerService {
 
   async getAvailableVoices(): Promise<AzureVoice[]> {
     // Return the same voice list as getVoices but without requiring API key
-    return this.getVoices('', '');
+    const voices: AzureVoice[] = [
+      // English voices
+      { Name: 'en-US-AvaMultilingualNeural', DisplayName: 'Ava', LocalName: 'Ava', ShortName: 'en-US-AvaMultilingualNeural', Gender: 'Female', Locale: 'en-US', Status: 'GA', SampleRateHertz: '24000', VoiceType: 'Neural' },
+      { Name: 'en-US-AndrewNeural', DisplayName: 'Andrew', LocalName: 'Andrew', ShortName: 'en-US-AndrewNeural', Gender: 'Male', Locale: 'en-US', Status: 'GA', SampleRateHertz: '24000', VoiceType: 'Neural' },
+      { Name: 'en-US-EmmaMultilingualNeural', DisplayName: 'Emma', LocalName: 'Emma', ShortName: 'en-US-EmmaMultilingualNeural', Gender: 'Female', Locale: 'en-US', Status: 'GA', SampleRateHertz: '24000', VoiceType: 'Neural' },
+      { Name: 'en-US-BrianNeural', DisplayName: 'Brian', LocalName: 'Brian', ShortName: 'en-US-BrianNeural', Gender: 'Male', Locale: 'en-US', Status: 'GA', SampleRateHertz: '24000', VoiceType: 'Neural' },
+      { Name: 'en-US-AriaNeural', DisplayName: 'Aria', LocalName: 'Aria', ShortName: 'en-US-AriaNeural', Gender: 'Female', Locale: 'en-US', Status: 'GA', SampleRateHertz: '24000', VoiceType: 'Neural' },
+      { Name: 'en-US-DavisNeural', DisplayName: 'Davis', LocalName: 'Davis', ShortName: 'en-US-DavisNeural', Gender: 'Male', Locale: 'en-US', Status: 'GA', SampleRateHertz: '24000', VoiceType: 'Neural' },
+      { Name: 'en-US-JaneNeural', DisplayName: 'Jane', LocalName: 'Jane', ShortName: 'en-US-JaneNeural', Gender: 'Female', Locale: 'en-US', Status: 'GA', SampleRateHertz: '24000', VoiceType: 'Neural' },
+      { Name: 'en-US-JasonNeural', DisplayName: 'Jason', LocalName: 'Jason', ShortName: 'en-US-JasonNeural', Gender: 'Male', Locale: 'en-US', Status: 'GA', SampleRateHertz: '24000', VoiceType: 'Neural' },
+      { Name: 'en-US-JennyMultilingualNeural', DisplayName: 'Jenny', LocalName: 'Jenny', ShortName: 'en-US-JennyMultilingualNeural', Gender: 'Female', Locale: 'en-US', Status: 'GA', SampleRateHertz: '24000', VoiceType: 'Neural' },
+      { Name: 'en-US-GuyNeural', DisplayName: 'Guy', LocalName: 'Guy', ShortName: 'en-US-GuyNeural', Gender: 'Male', Locale: 'en-US', Status: 'GA', SampleRateHertz: '24000', VoiceType: 'Neural' },
+      { Name: 'en-US-SaraNeural', DisplayName: 'Sara', LocalName: 'Sara', ShortName: 'en-US-SaraNeural', Gender: 'Female', Locale: 'en-US', Status: 'GA', SampleRateHertz: '24000', VoiceType: 'Neural' },
+      { Name: 'en-US-TonyNeural', DisplayName: 'Tony', LocalName: 'Tony', ShortName: 'en-US-TonyNeural', Gender: 'Male', Locale: 'en-US', Status: 'GA', SampleRateHertz: '24000', VoiceType: 'Neural' },
+      
+      // German voices
+      { Name: 'de-DE-FlorianNeural', DisplayName: 'Florian', LocalName: 'Florian', ShortName: 'de-DE-FlorianNeural', Gender: 'Male', Locale: 'de-DE', Status: 'GA', SampleRateHertz: '24000', VoiceType: 'Neural' },
+      { Name: 'de-DE-KatjaNeural', DisplayName: 'Katja', LocalName: 'Katja', ShortName: 'de-DE-KatjaNeural', Gender: 'Female', Locale: 'de-DE', Status: 'GA', SampleRateHertz: '24000', VoiceType: 'Neural' },
+      { Name: 'de-DE-ConradNeural', DisplayName: 'Conrad', LocalName: 'Conrad', ShortName: 'de-DE-ConradNeural', Gender: 'Male', Locale: 'de-DE', Status: 'GA', SampleRateHertz: '24000', VoiceType: 'Neural' },
+      { Name: 'de-DE-AmalaNeural', DisplayName: 'Amala', LocalName: 'Amala', ShortName: 'de-DE-AmalaNeural', Gender: 'Female', Locale: 'de-DE', Status: 'GA', SampleRateHertz: '24000', VoiceType: 'Neural' },
+      
+      // French voices
+      { Name: 'fr-FR-DeniseNeural', DisplayName: 'Denise', LocalName: 'Denise', ShortName: 'fr-FR-DeniseNeural', Gender: 'Female', Locale: 'fr-FR', Status: 'GA', SampleRateHertz: '24000', VoiceType: 'Neural' },
+      { Name: 'fr-FR-HenriNeural', DisplayName: 'Henri', LocalName: 'Henri', ShortName: 'fr-FR-HenriNeural', Gender: 'Male', Locale: 'fr-FR', Status: 'GA', SampleRateHertz: '24000', VoiceType: 'Neural' },
+      
+      // Spanish voices
+      { Name: 'es-ES-ElviraNeural', DisplayName: 'Elvira', LocalName: 'Elvira', ShortName: 'es-ES-ElviraNeural', Gender: 'Female', Locale: 'es-ES', Status: 'GA', SampleRateHertz: '24000', VoiceType: 'Neural' },
+      { Name: 'es-ES-AlvaroNeural', DisplayName: 'Alvaro', LocalName: 'Alvaro', ShortName: 'es-ES-AlvaroNeural', Gender: 'Male', Locale: 'es-ES', Status: 'GA', SampleRateHertz: '24000', VoiceType: 'Neural' },
+      
+      // Italian voices
+      { Name: 'it-IT-ElsaNeural', DisplayName: 'Elsa', LocalName: 'Elsa', ShortName: 'it-IT-ElsaNeural', Gender: 'Female', Locale: 'it-IT', Status: 'GA', SampleRateHertz: '24000', VoiceType: 'Neural' },
+      { Name: 'it-IT-DiegoNeural', DisplayName: 'Diego', LocalName: 'Diego', ShortName: 'it-IT-DiegoNeural', Gender: 'Male', Locale: 'it-IT', Status: 'GA', SampleRateHertz: '24000', VoiceType: 'Neural' },
+      
+      // Portuguese voices
+      { Name: 'pt-BR-FranciscaNeural', DisplayName: 'Francisca', LocalName: 'Francisca', ShortName: 'pt-BR-FranciscaNeural', Gender: 'Female', Locale: 'pt-BR', Status: 'GA', SampleRateHertz: '24000', VoiceType: 'Neural' },
+      { Name: 'pt-BR-AntonioNeural', DisplayName: 'Antonio', LocalName: 'Antonio', ShortName: 'pt-BR-AntonioNeural', Gender: 'Male', Locale: 'pt-BR', Status: 'GA', SampleRateHertz: '24000', VoiceType: 'Neural' },
+      
+      // Dutch voices
+      { Name: 'nl-NL-FennaNeural', DisplayName: 'Fenna', LocalName: 'Fenna', ShortName: 'nl-NL-FennaNeural', Gender: 'Female', Locale: 'nl-NL', Status: 'GA', SampleRateHertz: '24000', VoiceType: 'Neural' },
+      { Name: 'nl-NL-MaartenNeural', DisplayName: 'Maarten', LocalName: 'Maarten', ShortName: 'nl-NL-MaartenNeural', Gender: 'Male', Locale: 'nl-NL', Status: 'GA', SampleRateHertz: '24000', VoiceType: 'Neural' },
+      
+      // Polish voices
+      { Name: 'pl-PL-AgnieszkaNeural', DisplayName: 'Agnieszka', LocalName: 'Agnieszka', ShortName: 'pl-PL-AgnieszkaNeural', Gender: 'Female', Locale: 'pl-PL', Status: 'GA', SampleRateHertz: '24000', VoiceType: 'Neural' },
+      { Name: 'pl-PL-MarekNeural', DisplayName: 'Marek', LocalName: 'Marek', ShortName: 'pl-PL-MarekNeural', Gender: 'Male', Locale: 'pl-PL', Status: 'GA', SampleRateHertz: '24000', VoiceType: 'Neural' },
+      
+      // Romanian voices
+      { Name: 'ro-RO-AlinaNeural', DisplayName: 'Alina', LocalName: 'Alina', ShortName: 'ro-RO-AlinaNeural', Gender: 'Female', Locale: 'ro-RO', Status: 'GA', SampleRateHertz: '24000', VoiceType: 'Neural' },
+      { Name: 'ro-RO-EmilNeural', DisplayName: 'Emil', LocalName: 'Emil', ShortName: 'ro-RO-EmilNeural', Gender: 'Male', Locale: 'ro-RO', Status: 'GA', SampleRateHertz: '24000', VoiceType: 'Neural' },
+      
+      // Danish voices
+      { Name: 'da-DK-ChristelNeural', DisplayName: 'Christel', LocalName: 'Christel', ShortName: 'da-DK-ChristelNeural', Gender: 'Female', Locale: 'da-DK', Status: 'GA', SampleRateHertz: '24000', VoiceType: 'Neural' },
+      { Name: 'da-DK-JeppeNeural', DisplayName: 'Jeppe', LocalName: 'Jeppe', ShortName: 'da-DK-JeppeNeural', Gender: 'Male', Locale: 'da-DK', Status: 'GA', SampleRateHertz: '24000', VoiceType: 'Neural' },
+    ];
+    return voices;
   }
 
   async getVoices(apiKey: string, region: string): Promise<AzureVoice[]> {
